d26c708afa1c137aab28de9bb31c9375391679c7
jung.jin <sjn3649@gmail.com>
2026-05-19 15:40:33 +0900

    feature: 게시글 목록 관리 방식 수정 - 게시글 리스트 삭제, dict 단일 관리 - id 수치를 통해 Auto Increase 형태로 수정

--- 02주차/01번 과제/services/post_service.py
+++ 02주차/01번 과제/services/post_service.py
@@ -3,7 +3,7 @@ from schemas import PostBase, PostRead
 
 
 class PostService:
-    post_dict = {
+    _post_dict = {
         1: PostRead(
             id=1, title="제목 1", content="내용 1", writer="작성자 1", view_count=0
         ),
@@ -14,15 +14,15 @@ class PostService:
             id=3, title="제목 3", content="내용 3", writer="작성자 3", view_count=0
         ),
     }
-    post_list = list(post_dict.values())
+
+    _max_post_id = 3
 
     def add_new_post(self, post_base: PostBase):
-        new_id = self.post_list[-1].id + 1 if self.post_list else 1
+        PostService._max_post_id += 1
+        new_id = PostService._max_post_id
 
         post = PostRead(id=new_id, view_count=0, **post_base.model_dump())
-
-        self.post_dict[new_id] = post
-        self.post_list.append(post)
+        self._post_dict[new_id] = post
 
         return post
 
@@ -32,39 +32,29 @@ class PostService:
         updated_post = PostRead(
             id=post_id,
             view_count=old_post.view_count,
-            **post_update.model_dump(exclude={"id"}),
+            **post_update.model_dump(),
         )
 
-        self.post_dict[post_id] = updated_post
-
-        for idx, post in enumerate(self.post_list):
-            if post.id == post_id:
-                self.post_list[idx] = updated_post
-                break
-
+        self._post_dict[post_id] = updated_post
         return updated_post
 
     def delete_post(self, post_id: int):
-        if post_id not in self.post_dict:
+        if post_id not in self._post_dict:
             raise HTTPException(status_code=404, detail="Post not found")
 
-        del self.post_dict[post_id]
-        self.post_list = [post for post in self.post_list if post.id != post_id]
+        del self._post_dict[post_id]
 
     def get_post_detail(self, post_id: int):
-        post = self.post_dict.get(post_id)
+        post = self._post_dict.get(post_id)
         if not post:
             raise HTTPException(status_code=404, detail="Post not found")
         return post
 
     def get_all_post(self):
-        return self.post_list
+        return list(self._post_dict.values())
 
     def update_post_view_count(self, post_id: int, view_count: int):
-        post = self.post_dict.get(post_id)
-        if not post:
-            raise HTTPException(status_code=404, detail="Post not found")
-
+        post = self.get_post_detail(post_id)
         post.view_count = view_count
 
 
