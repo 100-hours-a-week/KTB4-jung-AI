9f2d1fcf2698ffae5288d57bc6c62b481ea60207
jung.jin <sjn3649@gmail.com>
2026-05-19 16:59:00 +0900

    refactor: modify comment list initialize logic - Change logic from comprehesion based  to foreach based

--- 02주차/01번 과제/services/comment_service.py
+++ 02주차/01번 과제/services/comment_service.py
@@ -1,8 +1,10 @@
 import json
 from pathlib import Path
+
 from fastapi import HTTPException
-from services.post_service import post_service
 from schemas import Comment, CommentCreate
+
+from services.post_service import post_service
 
 DATA_PATH = Path(__file__).resolve().parent.parent / "data" / "comments.json"
 
@@ -10,7 +12,11 @@ _initial_comments = {}
 if DATA_PATH.exists():
     with open(DATA_PATH, "r", encoding="utf-8") as f:
         _data = json.load(f)
-        _initial_comments = {int(k): [Comment(**c) for c in v] for k, v in _data.items()}
+        for post_id, comment_list in _data.items():
+            parsed_comments = []
+            for comment in comment_list:
+                parsed_comments.append(Comment(**comment))
+            _initial_comments[int(post_id)] = parsed_comments
 
 
 class CommentService:
@@ -18,26 +24,23 @@ class CommentService:
 
     def add_comment(self, post_id: int, comment_create: CommentCreate):
         post_service.get_post_detail(post_id)
-        
+
         comments = self._comment_dict.setdefault(post_id, [])
         new_comment_id = len(comments) + 1
-        
-        comment = Comment(
-            id=new_comment_id,
-            **comment_create.model_dump()
-        )
+
+        comment = Comment(id=new_comment_id, **comment_create.model_dump())
         comments.append(comment)
-        
+
         return comment
 
     def delete_comment(self, post_id: int, comment_id: int):
         post_service.get_post_detail(post_id)
-        
+
         comments = self._comment_dict.setdefault(post_id, [])
         comment_exists = any(c.id == comment_id for c in comments)
         if not comment_exists:
             raise HTTPException(status_code=404, detail="Comment not found")
-            
+
         self._comment_dict[post_id] = [c for c in comments if c.id != comment_id]
 
     def get_comments(self, post_id: int):
